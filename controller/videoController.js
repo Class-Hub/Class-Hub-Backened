3e5b515e8a767d68e44cc886bd348dfe408962e2
Bharat Kumar <kumarbharat661@gmail.com>
2021-10-29 14:57:06 +0530

    Changed the name option of req

--- controller/videoController.js
+++ controller/videoController.js
@@ -6,10 +6,10 @@ const VideoDetails = require('../models/VideoDetail');
 const uploadVideo = (req,res,next) => {
   console.log(req.userData)
     const videoDetails = new VideoDetails({
-        uploader_name: req.uploader_name,
+        uploader_name: req.headers.uploader_name,
         upload_title: req.file.filename.replace(/ /g, '_'),
         video_path: 'http://localhost:'+ process.env.PORT + '/videos/' + req.file.filename.replace(/ /g, '_'),
-        thumbnail_path: 'http://localhost:'+ process.env.PORT + '/thumbnail/' + req.userData.subject + '.jpg'
+        thumbnail_path: 'http://localhost:'+ process.env.PORT + '/thumbnail/' + 'DE.jpg'
       });
       videoDetails
       .save()
