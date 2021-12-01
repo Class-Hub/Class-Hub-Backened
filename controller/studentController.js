269bfbbb529d86fd08d07c638c8633ffab3baf5b
Prakhar <srivastava11735@gmail.com>
2021-12-01 23:53:44 +0530

    Password Updation Route

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,3 +1,5 @@
+const bcrypt = require("bcrypt");
+
 exports.profile = (req, res) => {
   try {
     const user = req.user;
@@ -8,6 +10,33 @@ exports.profile = (req, res) => {
     res.json({
       user,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(error);
+  }
+};
+
+exports.passwordUpdate = async (req, res) => {
+  const user = req.user;
+  const saltRounds = 10;
+  const { currentPassword, newPassword } = req.body;
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(404).send("User Not Found");
+    }
+
+    const match = await bcrypt.compare(currentPassword, user.password);
+    console.log(match);
+
+    if (match) {
+      const hash = bcrypt.hashSync(newPassword, saltRounds);
+      user.password = hash;
+      user.save();
+      res.json({
+        user,
+      });
+    }
   } catch (error) {
     console.log(error);
     res.status(error);
--- routes/student.js
+++ routes/student.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { authPass } = require("../controller/authController");
-const { profile } = require("../controller/studentController");
+const { profile, passwordUpdate } = require("../controller/studentController");
 const router = express.Router();
 const { markAttendance } = require("../controller/attendance");
 
 router.get("/", authPass, profile);
+router.post("/updatePassword", authPass, passwordUpdate);
 router.post("/mark", authPass, markAttendance);
 
 module.exports = router;
