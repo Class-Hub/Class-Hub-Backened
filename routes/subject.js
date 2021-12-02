f6accf21b7208c2d5159ce6d80aeecea35e9cbd4
Shivansh Joshi <77457296+sniperzee2@users.noreply.github.com>
2021-12-02 12:37:17 +0530

    Subject

--- routes/subject.js
+++ routes/subject.js
@@ -10,4 +10,15 @@ router.get("/", async (req, res) => {
   res.json({ subjects });
 });
 
+router.post("/getSubject", async (req, res) => {
+  const subjectId = req.body.subjectId;
+  const subject = await Subject.findById(subjectId).populate("students").populate("teachers");
+  if(subject) {
+    res.json({ subject });
+  }
+  else{
+    res.json({ message: "Subject not found" });
+  }
+})
+
 module.exports = router;
